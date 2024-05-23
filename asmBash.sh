da6bee8bbc1417cae65f5474b136ae16da4025ef
VojinMiletic <108096675+VojinMiletic@users.noreply.github.com>
2024-05-23 15:08:00 +0200

    Add files via upload

---
+++ asmBash.sh
@@ -0,0 +1,8 @@
+
+g++ -g ./src/Direktiva.cpp ./src/Instrukcija.cpp ./src/helper.cpp ./src/LeksickiElement.cpp ./src/Operand.cpp ./src/Asembler.cpp ./src/main.cpp ./misc/parser.cpp ./misc/lexer.cpp -o asembler
+
+if [ $? -eq 0 ]; then
+  echo "Uspeh. Izvrsna datoteka: asembler"
+else
+  echo "Greska prilikom kompajliranja"
+fi
